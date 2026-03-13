e280dcbc7aaa6fb8fc45c870a7bf92fb1fc9bacb
Huu Phuoc <phuocgagamer@gmail.com>
2026-03-13 08:17:46 +0700

    Thêm controller Category và Product

---
+++ TranHuuPhuoc_2123110236/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranHuuPhuoc_2123110236.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
