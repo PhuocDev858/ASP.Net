aef2885ae82264cdd2a1600afaf1522a62345371
Huu Phuoc <phuocgagamer@gmail.com>
2026-03-20 10:57:56 +0700

    Thiết lập kết nối cơ sở dữ liệu SQL Server, thêm bảng Products xuống db

---
+++ TranHuuPhuoc_2123110236/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TranHuuPhuoc_2123110236.Models;
+namespace TranHuuPhuoc_2123110236.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor này bắt buộc phải có để nhận Connection String từ Program.cs
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ TranHuuPhuoc_2123110236/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace TranHuuPhuoc_2123110236.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
